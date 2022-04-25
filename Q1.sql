fb808ae801f81d7a1bef3adbd00b61cc3b7b51f8
Jean Cloarec <66326709+kerpa@users.noreply.github.com>
2022-04-25 09:58:18 +0200

    correction 1
    correct severals mistake and improve the general overview of documents.

--- Q1.sql
+++ Q1.sql
@@ -6,23 +6,35 @@ Q1. Work with the sharing bike dataset: Sharing_Bike.db
  A. (4 pts) Considering the holidays, write a SQL code to calculate the total count of casual users and the
 total count of registered users in each holiday of 2012.
  */
-SELECT sum(casual) as total_casual, sum(registered) as total_registered
+
+/*
+ First I look at holidays aspect, if there is any cluster of days.
+ */
+SELECT dteday, holiday
+FROM Calendar
+WHERE holiday = 1;
+
+/*
+ Because there is no cluster of days, we can make the hypothesis that each day of holiday is a period of holidays.
+ Therefore, we can group by day to group by holidays
+ */
+
+SELECT bike.dteday,sum(casual) as total_casual, sum(registered) as total_registered
 FROM bike
 JOIN Calendar C on bike.dteday = C.dteday
-WHERE C.holiday = 1;
-/*
- a group by holidays or day is wrong because it will not give each holidays but each day of holidays.
- */
+WHERE C.holiday = 1
+GROUP BY bike.dteday;
+
 
 /*
 B. (4 pts) Considering 24 hours in a day, write a SQL code to calculate the average count of casual users
 and the average count of registered users for each hour.
  */
 /*
- I use 365 instead of count(*) because it is the number of days in a year and I verified that the number of days is 365
- for this database.
+ Display of total number of bikes used divided by the number of days in the database for each hour.
  */
-SELECT hr,(sum(casual)/365) as average_total_casual_by_hour, (sum(registered)/365) as average_total_registered_by_hour
+SELECT hr,(sum(casual)/COUNT ( DISTINCT dteday )) as average_total_casual_by_hour,
+       (sum(registered)/COUNT ( DISTINCT dteday )) as average_total_registered_by_hour
 FROM bike
 GROUP BY hr;
 /*
